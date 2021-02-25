dcdc262c2df21ed4a874bea06d037d5c387e4a04
Prenses Metin <60544998+PrensesMTN@users.noreply.github.com>
2021-02-25 19:37:09 +0300

    print_fonk oluşturuldu.

---
+++ print_fonk.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+// print fonksiyonunu tanımak için yapılan çalışmalar.
+int main(){
+	
+	printf("merhaba\nDünya");
+	printf("\n");
+	printf("\\n = m");
+	printf("\n");
+	printf("Gifted dunyaya \'merhaba\' dedi");
+	printf("\n");
+	printf("%c",'\'');
+	printf("\n");
+	printf("\?");
+	return 0;
+}
